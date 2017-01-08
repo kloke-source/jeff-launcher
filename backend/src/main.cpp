ed7e3939d7bf43b611d5f909cb7597f7c6b28b4f
Tashrif Sanil <tashrifsanil@kloke-source.com>
2017-01-07 19:05:36 -0500

    Added initial support for OS detection

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -5,5 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-  util::initialize();
+
+#ifdef __APPLE__
+#include "TargetConditionals.h"
+#if TARGET_OS_MAC
+  std::cout << "Apple platform" << std::endl;
+#else
+#   error "Unknown Apple platform"
+#endif
+#elif __linux__
+  // linux
+  std::cout << "Linux Platform" << std::endl;
+#else
+#   error "Unknown compiler"
+#endif
+
 }
